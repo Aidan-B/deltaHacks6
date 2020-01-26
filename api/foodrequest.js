3dc38480d8bad0bf844dc6cf8b05cf5d6acde399
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2020-01-26 07:12:48 -0500

    Api suggestions command created

--- api/foodrequest.js
+++ api/foodrequest.js
@@ -1,3 +1,6 @@
+//To run this, use the below command:
+//getFood("bread", ["vegan", "wheat-free", "and you dont know the keys dummy"], "").then((response) => {console.log(response)}).catch((response) => {console.log(response.response.data); return false});
+
 const axios = require('axios')
 const qs = require('querystring')
 
@@ -10,46 +13,81 @@ const key = "3652335158e41edd06f398a76a5e9e90"
 //calories is a range, formmated as: MIN+, MIN-MAX, or MAX
 
 //returns a json promise
-export function requestFood(foodname, health, calories) {
-    const baseUrl = "https://api.edamam.com/api/food-database/parser"
-    const vars = {
-        ingr: foodname,
-        app_id: id,
-        app_key: key
+export async function requestFood(foodname, health, calories) {
+    try {
+        const baseUrl = "https://api.edamam.com/api/food-database/parser"
+        const vars = {
+            ingr: foodname,
+            app_id: id,
+            app_key: key
+        }
+        if (health != "") {
+            vars.health = health;
+        }
+        if (calories != "") {
+            vars.calories = calories;
+        }
+    
+        const queryString = "?" + qs.stringify(vars)
+        
+        return axios.get(baseUrl + queryString)
+    } catch (e) {
+        throw(e);
     }
-    if (health != "") {
-        vars.health = health;
-    }
-    if (calories != "") {
-        vars.calories = calories;
-    }
-
-    const queryString = "?" + qs.stringify(vars)
-    return axios.get(baseUrl + queryString)
 }
 
-export function checkItem(foodName, healthLabel) {
-    let exists = false;
+export async function getFood(foodName, healthLabel, calories = "") {
+    try {
+        let foodPromise = await requestFood(foodName, healthLabel, calories)
+        let items = [];
+        for (let i = 0; i < 3; i++) {
+            let nutrientsPromise = await collectNutrientsInfo(foodPromise.data.hints[i], healthLabel)
+        
+            var match = true;
+            var exists = [];
+            for (let i = 0; i < nutrientsPromise.data.healthLabels.length && !exists; i++) {
+                nutrientsPromise.data.healthLabels.forEach(element => {
+                    if (nutrientsPromise.data.healthLabels[i] == healthLabel.toUpperCase())
+                        exists[element] = true;
+                });
+                exists.forEach(element => {
+                    if (!element)
+                        match = false;
+                })
+            }
+                
+            if (match)
+                items.push(foodPromise.data.hints[i].food.label)
+       
+        };
+        return items;
 
-    axios.post('https://api.edamam.com/api/food-database/nutrients?&app_id=3dd635e1&app_key=3652335158e41edd06f398a76a5e9e90',
-        {
+    } catch (e) {
+        throw (e);
+    }
+}
+
+export async function collectNutrientsInfo(foodItem) {
+    try {
+        const vars = {
+            app_id: id,
+            app_key: key
+        };
+        const nutrientsUrl = "https://api.edamam.com/api/food-database/nutrients?" + qs.stringify(vars);
+        
+        const NutrientsRequest = {
             "ingredients": [
                 {
                     "quantity": 1,
-                    "measureURI": "http://www.edamam.com/ontologies/edamam.owl#Measure_unit",
-                    "foodId": "food_bnbh4ycaqj9as0a9z7h9xb2wmgat"
+                    "measureURI": foodItem.measures[0].uri,
+                    "foodId": foodItem.food.foodId
                 }
             ]
-        })
-
-        .then(function (response) {
-            for (let i = 0; i < response.data.hint[0]['healthLabels'].length; i++) {
-                if (response.data['hint'][0]['healthLabels'][0] == healthLabel)
-                    exists = true;
-            }
-            return exists;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        };
+        
+        return axios.post(nutrientsUrl, NutrientsRequest);
+    } catch(e) {
+        throw(e)
+    }
 }
+    
