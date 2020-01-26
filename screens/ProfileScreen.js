59532ab2cf6491fb9b297f8f0b29760bcecf979d
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2020-01-26 02:28:08 -0500

    Settings template started...

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -4,11 +4,25 @@ import { ExpoConfigView } from '@expo/samples';
 import styles from '../constants/styles.js'
 import { View, CheckBox, Text, Image, StyleSheet } from 'react-native';
 
+const Checkbox = props => (<input type="checkbox" {...props}/>)
+
+
+class CardHead extends Component {
+  render() {
+    return(
+      <View style={{margin: 20, marginBottom: 0, alignSelf: 'stretch', flexDirection: 'row', borderBottomColor: 'lightgrey', borderBottomWidth: 2}}>
+        <Text style={{fontSize: 24, alignSelf:"flex-end", flex: 1, textAlign:"right", textAlignVertical:"bottom", margin:5, marginBottom:0}}>{this.props.name}</Text>
+      </View>
+    )
+  }
+}
+
 class OptionCard extends Component {
   render() {
     return(
-      <View style={{height: 80}}>
-        <Text>{this.props.name}</Text>
+      <View style={{marginRight:10, alignSelf: 'flex-end', flexDirection: 'row', alignItems:"center"}}>
+        <Text style={{fontSize: 16, textAlign:"right", margin: 10}}>{this.props.name}</Text>
+        <CheckBox style={{margin:10}}></CheckBox>
       </View>
     )
   }
@@ -19,9 +33,10 @@ export default class ProfileScreen extends Component {
       return (
         <View style={styles.container}>
           <View style={{alignItems: "center"}}>
-            <OptionCard name='Working is hard' style={{backgroundColor: 'powderblue'}}></OptionCard>
-            <OptionCard name='Working is hard' style={{backgroundColor: 'skyblue'}}></OptionCard>
-            <OptionCard name='Working is hard' style={{backgroundColor: 'steelblue'}}></OptionCard>
+            <CardHead name="Diets"></CardHead>
+            <OptionCard name='Vegan' style={{backgroundColor: 'powderblue'}}></OptionCard>
+            <OptionCard name='Vegetarian' style={{backgroundColor: 'skyblue'}}></OptionCard>
+            <OptionCard name='Gluten Free' style={{backgroundColor: 'steelblue'}}></OptionCard>
           </View>
         </View>
       );
