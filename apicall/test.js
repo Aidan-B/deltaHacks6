9949b4efc9e0ed765c737d985a0bacb2098630af
Aidan-B <31750217+Aidan-B@users.noreply.github.com>
2020-01-26 00:04:23 -0500

    updated api with filters

--- apicall/test.js
+++ apicall/test.js
@@ -1,27 +1,58 @@
 const axios = require('axios')
 const qs = require('querystring')
 
-const id = "knc7bJh4"
-const secret = "TSX4W7lksxqgid74wJLh5G4raloErMMS"
+const id = "3dd635e1"
+const key = "3652335158e41edd06f398a76a5e9e90"
 
-function requestFood(foodname) {
-  const baseUrl = "https://api.iamdata.co/v1/products/"
+
+//Foodname is the name as a string
+//health is one of the pre-defined health categories
+//calories is a range, formmated as: MIN+, MIN-MAX, or MAX
+function requestFood(foodname, health, calories) {
+  const baseUrl = "https://api.edamam.com/api/food-database/parser"
   const vars = {
-    per_page: 50,
-    name: foodname,
-    client_id: id,
-    client_secret: secret
+    ingr: foodname,
+    app_id: id,
+    app_key: key
   }
+  if (health != "") {
+    vars.health = health;
+  }
+  if (calories != "") {
+    vars.calories = calories;
+  }
+
   const queryString = "?" + qs.stringify(vars)
   //console.log(queryString)
   return axios.get(baseUrl + queryString)
 }
 
-requestFood('bread').then((response) => {
+  
+name = "Bacon";
+health = ["vegan"];
+calories = ""
+
+requestFood(name, health, calories).then(function (response) {
+  console.log(response.data);
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+/*
+requestNutrition().then((response) => {
   const data = response.data
   console.log(data)
-  // console.log(data.result.map(x => x.name))
 })
 
+/*
+requestFood('bread').then((response) => {
+  const data = response.data
+  console.log(JSON.stringify(data))
+  // console.log(data.result.map(x => x.name))
+})
 // console.log([1, 2, 3].map(x => x * 2))
 // console.log([{ x: 5 }, { x: 2 }].map(y => y.x))
+*/
+
